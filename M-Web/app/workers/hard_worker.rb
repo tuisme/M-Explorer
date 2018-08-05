bb8abf5d55ebdb95276d6fd58fcdc215e0df27e5
KylePham96 <minhtu.bk14@gmail.com>
2018-08-05 14:24:10 +0700

    sidekiq worked!!! Awesome

--- M-Web/app/workers/hard_worker.rb
+++ M-Web/app/workers/hard_worker.rb
@@ -1,16 +1,39 @@
 class HardWorker
   include Sidekiq::Worker
 
-  def perform(name)
-    case name
-    when "b"
-      sleep 10
-      logger.info "Things are happening."
-      logger.debug "Here's some info: #{hash.inspect}"
-    else
-      sleep 1
-      logger.info "Things are happening."
-      logger.debug "Here's some info: #{hash.inspect}"
+  def perform()
+    success = 0
+    error = 0
+    Cloud.all.each do |cloud|
+      if cloud.cloud_type == 'googledrive'
+        @credentials = Google::Auth::UserRefreshCredentials.new(
+          client_id: '389228917380-ek9t84cthihvi8u4apphlojk3knd5geu.apps.googleusercontent.com',
+          client_secret: 'zhKkS-8vI_RNqReXOjAx4c5r',
+          scope: [
+            'https://www.googleapis.com/auth/drive'
+          ],
+          additional_parameters: {
+            'access_type' => 'offline',
+            'prompt' => 'consent'
+          },
+          redirect_uri: 'http://localhost:3000')
+        @credentials.refresh_token = cloud.refresh_token
+        access_token = @credentials.fetch_access_token!['access_token']
+        if cloud.update(access_token: access_token, updated_at: Time.now)
+          success += 1
+        else
+          error += 1
+        end
+      elsif cloud.cloud_type == 'box'
+        access_token = Boxr.refresh_tokens(cloud.refresh_token, client_id: 'i9jieqavbpuutnbbrqdyeo44m0imegpk', client_secret: '4LjQ7N3toXIXVozyXOB21tBTcCo2KX6F').access_token
+        refresh_token = Boxr.refresh_tokens(cloud.refresh_token, client_id: 'i9jieqavbpuutnbbrqdyeo44m0imegpk', client_secret: '4LjQ7N3toXIXVozyXOB21tBTcCo2KX6F').refresh_token
+        if cloud.update(access_token: access_token, refresh_token: refresh_token, updated_at: Time.now)
+          success += 1
+        else
+          error += 1
+        end
+      end
     end
+    puts 'Success: ' + success.to_s + ' | Error: ' + error.to_s
   end
 end
